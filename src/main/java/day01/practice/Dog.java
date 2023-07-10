f44d5c630ab56863b8f2e1e1ff8d6e47f7618830
deemoseenu <siva.gopi@fwsa.freshworks.com>
2023-07-10 17:14:14 +0530

    day 1 practice and solved

---
+++ src/main/java/day01/practice/Dog.java
@@ -0,0 +1,45 @@
+package day01.practice;
+
+public class Dog {
+    // Attributes
+    private boolean hasDots;
+    private String color;
+
+
+    // Getters and setters
+    public boolean hasDots() {
+        return hasDots;
+    }
+
+    public void setHasDots(boolean hasDots) {
+        this.hasDots = hasDots;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // Main method to create Dog objects
+    public static void main(String[] args) {
+        // Create two Dog objects
+        Dog dog1 = new Dog();
+        dog1.setColor("purple");
+        dog1.setHasDots(false);
+        Dog dog2 = new Dog();
+        dog2.setColor("blue");
+        dog2.setHasDots(true);
+
+        // Print the attributes of each Dog
+        System.out.println("Dog 1:");
+        System.out.println("Has Dots: " + dog1.hasDots());
+        System.out.println("Color: " + dog1.getColor());
+
+        System.out.println("Dog 2:");
+        System.out.println("Has Dots: " + dog2.hasDots());
+        System.out.println("Color: " + dog2.getColor());
+    }
+}
---
+++ src/main/java/day01/solved/InstanceVariable.java
@@ -0,0 +1,17 @@
+package day01.solved;
+
+public class InstanceVariable {
+	
+	 // Declare an instance variable
+    public int instanceVariable;
+
+    public static void main(String[] args) {
+        // Create an object of MyClass
+    	InstanceVariable myObject = new InstanceVariable();
+
+        // Access the instance variable using the object reference
+        myObject.instanceVariable = 5;
+        System.out.println(myObject.instanceVariable);
+    }
+
+}
